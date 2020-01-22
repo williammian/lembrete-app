0c507a62a705a35af768bef4f73a26f25ccd68a5
williammian <william_mian@yahoo.com.br>
2020-01-22 06:46:15 -0300

    componente mensagem de Erro - ViewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { ErrorMsgComponent } from './compartilhado/error-msg/error-msg.component';
+import { Component, ViewChild, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit{
   public titulo = 'CRUD de lembretes com Angular';
+  @ViewChild(ErrorMsgComponent, {static: false}) errorMsgComponent: ErrorMsgComponent;
+
+  ngOnInit() {
+
+  }
+
+  ngAfterViewInit() {
+    this.errorMsgComponent.setError('Esta é uma mensagem de erro.');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErrorMsgComponent } from './compartilhado/error-msg/error-msg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorMsgComponent
   ],
   imports: [
     BrowserModule,
