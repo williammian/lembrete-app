812875051ac810c0b31167890346c234efb5cda2
williammian <william_mian@yahoo.com.br>
2020-01-25 15:26:17 -0300

    criando componente da pagina de criacao

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
+import { CriarLembreteComponent } from './paginas/criar-lembrete/criar-lembrete.component';
 import { ListaLembreteComponent } from './paginas/lista-lembrete/lista-lembrete.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: '', component: ListaLembreteComponent }
+  { path: '', component: ListaLembreteComponent },
+  { path: 'lembrete/criar', component: CriarLembreteComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { ErrorMsgComponent } from './compartilhado/error-msg/error-msg.component';
 import { ListaLembreteComponent } from './paginas/lista-lembrete/lista-lembrete.component';
 import { FormLembreteComponent } from './compartilhado/form-lembrete/form-lembrete.component';
+import { CriarLembreteComponent } from './paginas/criar-lembrete/criar-lembrete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ErrorMsgComponent,
     ListaLembreteComponent,
-    FormLembreteComponent
+    FormLembreteComponent,
+    CriarLembreteComponent
   ],
   imports: [
     BrowserModule,
