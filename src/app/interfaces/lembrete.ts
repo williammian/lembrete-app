8bb79d609ac4de007a4ccb22211d96a040a078ad
williammian <william_mian@yahoo.com.br>
2020-01-25 15:50:48 -0300

    lembrete

--- src/app/interfaces/lembrete.ts
+++ src/app/interfaces/lembrete.ts
@@ -2,7 +2,5 @@ type prioridade = 'BAIXA' | 'MEDIA' | 'ALTA';
 export interface Lembrete {
   id: number;
   conteudo: string;
-  arquivado: boolean;
   prioridade: prioridade;
-  modificado: number;
 }
